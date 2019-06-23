2b3b126d0f9d74de38818abc17fa784de21a841a
Anthony Luc <anthonyvanluc@gmail.com>
2019-06-23 19:19:41 -0400

    init brew.sh file

---
+++ brew.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+# TODO: figure out how to include this
+
+# Essentials
+brew install \
+    bash \
+    cmake \
+    curl \
+    dos2unix \
+    fzf \
+    gcc \
+    gdb \
+    git \
+    glib \
+    gnupg \
+    go \
+    google-chrome \
+    grep \
+    netcat \
+    nmap \
+    node \
+    openssl \
+    python \
+    react-native-cli \
+    readline \
+    ripgrep \
+    rsync \
+    ruby \
+    tmux \
+    typescript \
+    vim \
+    wget \
+    zsh
+
+brew cask install \
+    alfred \
+    clipy \
+    iterm2 \
+    keepassxc \
+    google-backup-and-sync \
+    qlcolorcode \
+    qlstephen \
+    qlmarkdown \
+    qlvideo \
+    quicklook-csv \
+    quicklook-json \
+    slack \
+    spectacle \
+    spotify \
+    sublime-text \
+    visual-studio-code \
+    vlc
+
+# Personal
+brew install \
+    youtube-dl
+
+brew cask install \
+    calibre \
+    discord \
+    gimp \
+    minecraft \
+    steam \
+    telegram \
+    private-internet-access \
+    qbittorrent \
+    youtube-to-mp3
+
+# Optional
+brew install \
+    angular-cli \
+    awscli \
+    dropbox \
+    heroku \
+    hugo \
+    packer \
+    perl \
+    telnet \
+    weechat
+
+brew cask install \
+    aerial \
+    darktable \
+    docker \
+    fastlane \
+    firefox \
+    github \
+    minikube \
+    obs \
+    postman \
+    signal \
+    vagrant \
+    wireshark \
+    xquartz
+
